b3438cf0dfc20dfd5d1fc584ca1bd72abd82d928
KobOlek <94456327+KobOlek@users.noreply.github.com>
2024-11-04 21:49:30 +0200

    Raspberry Pi 5 update

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
+# imports
 import cv2
 import numpy as np
 from util import get_limits
 import mouse
-import ctypes
-import time
+from screeninfo import get_monitors
+import time, pyautogui
+from picamera2 import Picamera2
 
+# Connecting to Pi's camera
+camera = Picamera2()
+config = camera.create_preview_configuration({'format': 'RGB888'})
+camera.configure(config)
+camera.start()
 
-# IP camera url
-url = "http://192.168.143.54:8080/video" #"http://192.168.0.126:8080/video" #"http://192.168.31.74:8080/video"
-
-
+# Get colors
 def get_color(lower_color: np.array, upper_color: np.array):
     mask = cv2.inRange(hsv, lower_color, upper_color)
     (minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(mask)
@@ -20,7 +24,7 @@ def get_color(lower_color: np.array, upper_color: np.array):
 lower_laser = np.array([0, 0, 255])
 upper_laser = np.array([255, 255, 255])
 
-lower_red, upper_red = get_limits([255,0,0][::-1])
+lower_red, upper_red = get_limits([200,0,0][::-1])
 
 lower_blue, upper_blue = get_limits([0,0,255][::-1])
 
@@ -28,16 +32,10 @@ lower_green, upper_green = get_limits([0,255,0][::-1])
 
 
 # Calibration: screen size
-user32 = ctypes.windll.user32
-screen_width, screen_height = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
-
-
+screen_width, screen_height = pyautogui.size()[0], pyautogui.size()[1]
 # Video capturing
-cap = cv2.VideoCapture(url)
-
-#time.sleep(1)
 while True:
-    ret, frame = cap.read()
+    frame = camera.capture_array()
     if frame is None:
         break
 
@@ -60,7 +58,7 @@ while True:
     # Calibration: projecting image size
     field_width = maxGreenLoc[0] - maxBlueLoc[0]
     field_height = maxBlueLoc[1] - maxRedLoc[1]
-    
+
     # Calibration: cursor positioning
     if field_width > 0 and field_height > 0:
         width_k = screen_width / field_width
@@ -68,12 +66,10 @@ while True:
 
         red_x = maxLaserLoc[0] - maxRedLoc[0]
         red_y = maxLaserLoc[1] - maxRedLoc[1]
-
         if red_x >= 0 and red_y >= 0:
             cursor_x = (red_x) * width_k
             cursor_y = (red_y) * height_k
-
-            mouse.move(cursor_x, cursor_y)
+            mouse.move(int(cursor_x), int(cursor_y))
 
     # Camera frame showing
     cv2.imshow('Track Laser', frame)
@@ -82,5 +78,5 @@ while True:
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-cap.release()
+camera.stop()
 cv2.destroyAllWindows()
